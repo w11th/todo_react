a1a8497807da8940d9721279eddbfb3121623b5a
Wenhui Wang <w.eleventh@gmail.com>
2017-05-23 16:10:34 +0800

    don't use ref

--- src/todos/views/AddTodo.jsx
+++ src/todos/views/AddTodo.jsx
@@ -9,13 +9,17 @@ class AddTodo extends Component {
 
     this.refInput = this.refInput.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.state = {
+      value: ""
+    };
   }
 
   render() {
     return (
       <div className="add-todo">
         <form onSubmit={this.onSubmit}>
-          <input className="new-todo" ref={this.refInput}/>
+          <input className="new-todo" onChange={this.onInputChange} value={this.state.value}/>
           <button className="add-btn" type="submit">添加</button>
         </form>
       </div>
@@ -26,15 +30,23 @@ class AddTodo extends Component {
     this.input = node;
   }
 
+  onInputChange(ev) {
+    this.setState({
+      value: ev.target.value
+    });
+  }
+
   onSubmit(ev) {
     ev.preventDefault();
 
-    const input = this.input;
-    if (!input.value.trim()) {
+    const value = this.state.value;
+    if (!value.trim()) {
       return;
     }
-    this.props.onAdd(input.value);
-    input.value = '';
+    this.props.onAdd(value);
+    this.setState({
+      value: ""
+    });
   }
 }
 
