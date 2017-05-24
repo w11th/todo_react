ed75bf5bafb326933a0162c7c8adccac422face1
Wenhui Wang <w.eleventh@gmail.com>
2017-05-24 13:46:40 +0800

    Add reselect

--- src/todos/views/TodoList.jsx
+++ src/todos/views/TodoList.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-/* import {bindActionCreators} from 'redux';*/
 
 import TodoItem from './TodoItem.jsx';
-import {FilterTypes} from '../../constants.js';
+import {selectVisibleTodos} from '../selector.js';
 
 const TodoList = ({todos}) => {
   return (
@@ -27,50 +26,10 @@ TodoList.propTypes = {
   todos: PropTypes.array.isRequired,
 };
 
-const selectVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case FilterTypes.ALL: {
-      return todos;
-    }
-    case FilterTypes.COMPLETED: {
-      return todos.filter(item => item.completed);
-    }
-    case FilterTypes.UNCOMPLETED: {
-      return todos.filter(item => !item.completed);
-    }
-    default: {
-      throw new Error('unsupported filter');
-    }
-  }
-};
-
 const mapStateToProps = (state) => {
   return {
-    todos: selectVisibleTodos(state.todos, state.filter)
+    todos: selectVisibleTodos(state)
   }
 };
-
-/* const mapDispatchToProps = (dispatch) => {
- *   return {
- *     onToggleTodo: (id) => {
- *       dispatch(toggleTodo(id));
- *     },
- *     onRemoveTodo: (id) => {
- *       dispatch(removeTodo(id));
- *     }
- *   };
- * };
- * */
-/*
- * const mapDispatchToProps = (dispatch) => bindActionCreators({
- *   onToggleTodo: toggleTodo,
- *   onRemoveTodo: removeTodo
- * }, dispatch);*/
-/*
- * const mapDispatchToProps = {
- *   onToggleTodo: toggleTodo,
- *   onRemoveTodo: removeTodo
- * };
- * */
 
 export default connect(mapStateToProps)(TodoList);
