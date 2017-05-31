98b8ce7ebd62b8794767f773428bd28d12088b95
Wenhui Wang <w.eleventh@gmail.com>
2017-05-31 13:46:13 +0800

    Add transition group

--- src/todos/views/TodoList.jsx
+++ src/todos/views/TodoList.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import TransitionGroup from 'react-addons-css-transition-group';
 
 import TodoItem from './TodoItem.jsx';
 import {selectVisibleTodos} from '../selector.js';
+import './todoItem.css';
 
 const TodoList = ({todos}) => {
   return (
     <ul className="todo-list">
-      {
-        todos.map((item)=> (
-          <TodoItem
-            key={item.id}
-            id={item.id}
-            text={item.text}
-            completed={item.completed}
-          />
-        ))
-      }
+      <TransitionGroup transitionName="fade" transitionEnterTimeout={500} transitionLeaveTimeout={200}>
+        {
+          todos.map((item)=> (
+            <TodoItem
+              key={item.id}
+              id={item.id}
+              text={item.text}
+              completed={item.completed}
+            />
+          ))
+        }
+      </TransitionGroup>
     </ul>
   );
 };
